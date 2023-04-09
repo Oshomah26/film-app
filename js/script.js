00c0b1bbb5f78b9e5dd3f59bf8745d8062ebb5c6
Oshomah <oshomah26@gmail.com>
2023-04-09 13:45:11 +0100

    Added the TV details page with a few tweaks

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ async function displayMovieDetails(){
     // Overlay for background image
     displayBackgroundImage('movie', movie.backdrop_path);
 
-    const div = document. createElement('div');
+    const div = document.createElement('div');
 
     div.innerHTML = `
     <div class="details-top">
@@ -129,53 +129,71 @@ async function displayMovieDetails(){
       <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(',  ')}</div>
     </div>
   `
-//`
-//     <div>
-//     ${
-//         movie.poster_path ? `<img
-//         src="https://image.tmdb.org/t/p/w300${movie.poster_path}"
-//         class="card-img-top"
-//         alt="Movie Title"
-//       />`
-//       :
-//       `<img
-//       src="/film-app/images/no-image.jpg"
-//       class="card-img-top"
-//       alt="${movie.title}"
-//     />`
-//     }
-//     </div>
-//     <div>
-//       <h2>${movie.title}</h2>
-//       <p>
-//         <i class="fas fa-star text-primary"></i>
-//         ${movie.vote_average.toFixed(1)}/ 10
-//       </p>
-//       <p class="text-muted">Release Date: ${movie.release_date}</p>
-//       <p>
-//         ${movie.overview}
-//       </p>
-//       <h5>Genres</h5>
-//       <ul class="list-group">
-//         ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
-//       </ul>
-//       <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
-//     </div>
-    
-//   </div>
-//   <div class="details-bottom">
-//     <h2>Movie Info</h2>
-//     <ul>
-//       <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
-//       <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
-//       <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
-//       <li><span class="text-secondary">Status:</span> ${movie.status}</li>
-//     </ul>
-//     <h4>Production Companies</h4>
-//     <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`)}</div>
-// `;
 
 document.querySelector('#movie-details').appendChild(div);
+
+
+}
+
+// Display show details
+
+async function displayShowDetails(){
+  const showId = window.location.search.split('=')[1];
+    
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('tv', show.backdrop_path);
+
+    const div = document. createElement('div');
+
+    div.innerHTML = `
+    <div class="details-top">
+      <div>
+      ${
+                show.poster_path ? `<img
+                src="https://image.tmdb.org/t/p/w300${show.poster_path}"
+                class="card-img-top"
+                alt="show Title"
+              />`
+              :
+              `<img
+              src="/film-app/images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+      </div>
+      <div>
+        <h2>${show.name}</h2>
+        <p>
+          <i class="fas fa-star text-primary"></i>
+          ${show.vote_average.toFixed(1)}/ 10
+        </p>
+        <p class="text-muted">Last air date: ${show.last_air_date}</p>
+        <p>
+        ${show.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+      </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Show Info</h2>
+      <ul>
+      <li><span class="text-secondary">Number of episodes:</span> ${show.number_of_episodes}</li>
+           <li><span class="text-secondary">Last Episode to Air:</span> ${show.last_episode_to_air.name}</li>
+           <li><span class="text-secondary">Status:</span> ${show.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${show.production_companies.map((company) => `<span>${company.name}</span>`).join(',  ')}</div>
+    </div>
+  `
+
+document.querySelector('#show-details').appendChild(div);
 
 
 }
@@ -255,7 +273,7 @@ function init(){
             displayMovieDetails();
             break;
         case '/film-app/tv-details.html':
-            console.log('Film Dettails')
+            displayShowDetails();
             break;
         case '/film-app/search.html':
             console.log('Search');
